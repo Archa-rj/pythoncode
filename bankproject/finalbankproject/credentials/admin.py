d85956d71b291b1d7268fc76980b1a6a1b1a5ab3
Archa rj <147069137+Archa-rj@users.noreply.github.com>
2023-11-22 12:16:10 +0530

    Delete bankproject/finalbankproject directory

--- bankproject/finalbankproject/credentials/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Office, District, Person
-
-admin.site.register(Office)
-admin.site.register(District)
-admin.site.register(Person)
